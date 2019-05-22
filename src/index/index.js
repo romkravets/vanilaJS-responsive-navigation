942fc39aa579e8f90c1f30eb66137546d5b858e5
romkravets <romann.kravets@gmail.com>
2019-05-22 20:49:22 +0300

    Added class NavMenus

--- src/index/index.js
+++ src/index/index.js
@@ -1,5 +1,5 @@
 import './index.scss';
 import { NavMenu } from './scripts/NavMenu';
 
-new NavMenu(document.querySelector('#navigation'));
+new NavMenu(document.querySelector('#navigation'), [ "HOME", "PORTFOLIO", "SERVICE", "CONTACT US"]);
 
--- src/index/scripts/NavMenu.js
+++ src/index/scripts/NavMenu.js
@@ -1,8 +1,32 @@
-export class NavMenu {
-   constructor(rootElenent) {
-      console.log(rootElenent);
-   }
-   render() {
+import '../styles/NavMenu.scss';
 
+export class NavMenu {
+   constructor(rootElenent, options) {
+      this.rootElenent = rootElenent;
+      this.options = options;
+      this.items;
+      this.item;
+      this.li;
+      this.render();
+   }
+
+   render() {
+      this.items = document.createElement('ul');
+      this.items.classList.add('nav__items');
+
+      this.rootElenent.appendChild(this.items);
+
+      for(let i = 0; i < this.options.length; i++) {
+          this.li = document.createElement('li');
+          this.option = document.createElement('a');
+   
+          this.li.classList.add('nav__item');
+          this.option.classList.add('nav__link');
+          this.option.setAttribute("href", "#");
+            this.option.textContent = this.options[i];
+   
+         this.items.appendChild(this.li);
+         this.li.appendChild(this.option);
+       }
    }
 }
